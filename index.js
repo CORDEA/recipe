370edccefbdfd21530b29e10e3e3189d709f9147
Yoshihiro Tanaka <contact@cordea.jp>
2021-12-05 12:57:00 +0900

    add button

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import './index.scss';
 import {MDCTextField} from '@material/textfield';
+import {MDCRipple} from '@material/ripple';
 
 const fields = document.querySelectorAll('.mdc-text-field');
 fields.forEach((e) => new MDCTextField(e));
+new MDCRipple(document.querySelector('.mdc-button'));
